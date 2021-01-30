e983e9c4828516d5be79f81eefea7f0805a27045
zsl50 <zsl50@ZSL503>
2021-01-30 14:30:14 +0800

    添加 .gitignore 和 .gitattributes。

---
+++ Package/ExcelCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 排课助手.Package
+{
+	public class ExcelCell
+	{
+		public string text { get; set; }
+	}
+}
